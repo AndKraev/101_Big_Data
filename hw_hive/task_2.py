c6a7b6f39eb1825bc752621d6a8f9edf66df9063
Andrey <akrstp@gmail.com>
2021-11-11 23:28:53 +0300

    init

---
+++ hw_hive/task_2.py
@@ -0,0 +1,3 @@
+"""Find the most popular country where hotels are booked and searched from the same
+country. Implement using scala or python. Create a separate application. Copy the
+application to the archive. Make screenshots of results: before and after execution. """
