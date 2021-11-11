5ea93a1b856f1648d0efbe04bbdc2433f115cde4
Andrey <akrstp@gmail.com>
2021-11-12 00:17:17 +0300

    save work

--- hw_hive/task_2.py
+++ hw_hive/task_2.py
@@ -1,3 +1,28 @@
 """Find the most popular country where hotels are booked and searched from the same
 country. Implement using scala or python. Create a separate application. Copy the
 application to the archive. Make screenshots of results: before and after execution. """
+from pyspark.sql import SparkSession
+from task_1 import build_hotel_dataframe
+
+
+def most_popular_search_and_booked_country(dataframe):
+    (
+        dataframe.filter(
+            dataframe["user_location_country"] == dataframe["hotel_country"]
+        )
+        .select("user_location_country")
+        .groupBy("user_location_country")
+        .count()
+        .agg({"count": "max"})
+        .show()
+    )
+
+
+if __name__ == "__main__":
+    spark = (
+        SparkSession.builder.master("local[*]")
+        .appName("PySpark_Tutorial")
+        .getOrCreate()
+    )
+    filepath = "E:\\PyEducation\\101 Big Data\\expedia-hotel-recommendations\\train.csv"
+    most_popular_search_and_booked_country(build_hotel_dataframe(spark, filepath))
