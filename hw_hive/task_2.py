eeceb29cd7b99f8e07c06ca76f66344d6fd3f99a
Andrey <akrstp@gmail.com>
2021-11-12 13:26:52 +0300

    finish task2

--- hw_hive/task_2.py
+++ hw_hive/task_2.py
@@ -1,20 +1,51 @@
 """Find the most popular country where hotels are booked and searched from the same
 country. Implement using scala or python. Create a separate application. Copy the
 application to the archive. Make screenshots of results: before and after execution. """
+import time
+
 from pyspark.sql import SparkSession
 from task_1 import build_hotel_dataframe
 
 
-def most_popular_search_and_booked_country(dataframe):
+def most_popular_search_and_booked_country_with_agg(dataframe):
+    """Find the most popular country where hotels are booked and searched from the same
+    country with an aggregation.
+
+    :param dataframe: A Spark Dataframe with hotels
+    :type dataframe: Dataframe
+    :return: None
+    :rtype: NoneType
+    """
+    filtered_df = (
+        dataframe.filter(
+            dataframe["user_location_country"] == dataframe["hotel_country"]
+        )
+        .select("user_location_country", "hotel_country")
+        .groupBy("user_location_country")
+        .count()
+    )
+    maximum = filtered_df.agg({"count": "max"}).collect()[0]["max(count)"]
+    filtered_df.filter(filtered_df["count"] == maximum).show()
+
+
+def most_popular_search_and_booked_country_with_sort(dataframe):
+    """Find the most popular country where hotels are booked and searched from the same
+    country with a sort transformation.
+
+    :param dataframe: A Spark Dataframe with hotels
+    :type dataframe: Dataframe
+    :return: None
+    :rtype: NoneType
+    """
     (
         dataframe.filter(
             dataframe["user_location_country"] == dataframe["hotel_country"]
         )
-        .select("user_location_country")
+        .select("user_location_country", "hotel_country")
         .groupBy("user_location_country")
         .count()
-        .agg({"count": "max"})
-        .show()
+        .sort("count", ascending=False)
+        .show(1)
     )
 
 
@@ -25,4 +56,17 @@ if __name__ == "__main__":
         .getOrCreate()
     )
     filepath = "E:\\PyEducation\\101 Big Data\\expedia-hotel-recommendations\\train.csv"
-    most_popular_search_and_booked_country(build_hotel_dataframe(spark, filepath))
+
+    # Try with sort
+    start_time = time.time()
+    most_popular_search_and_booked_country_with_sort(
+        build_hotel_dataframe(spark, filepath)
+    )
+    print(f"--- Time with sort: {time.time() - start_time} seconds ---")
+
+    # Try with agg
+    start_time = time.time()
+    most_popular_search_and_booked_country_with_agg(
+        build_hotel_dataframe(spark, filepath)
+    )
+    print(f"--- Time with agg: {time.time() - start_time} seconds ---")
